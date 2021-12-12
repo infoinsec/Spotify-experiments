f1d1ff0aa480ceecc89e2eee5b843ce48dab41b3
infoinsec <53850514+infoinsec@users.noreply.github.com>
2021-12-12 12:30:08 -0500

    Create getMyFavoriteSongs.js

---
+++ getMyFavoriteSongs.js
@@ -0,0 +1,3 @@
+//auth token scoped to only user-library-read access:
+//Authorization: Bearer BQBEDdtJIPOiWvr5NvTBZ3BJ3BJ5Rpm6Tf3HWqCMSHDGuz3WBeuIhz1nMvB1MOsYc_0a-uyYqjtpT0qYU7C9OjgHebUd4BpISG4Y7jHAO3cRWGJmrNpLmrBmn2NM9x9jQsa1f7lIJwUY73jxURs8kNJnTw
+
