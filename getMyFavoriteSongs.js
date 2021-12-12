04a6727928bb72d9ed55f30a747cb5f8747c8d64
Alex Balliet <alexballiet+infoinsec@gmail.com>
2021-12-12 12:56:41 -0500

    added node-fetch

--- getMyFavoriteSongs.js
+++ getMyFavoriteSongs.js
@@ -1,3 +1,9 @@
 //auth token scoped to only user-library-read access:
 //Authorization: Bearer BQBEDdtJIPOiWvr5NvTBZ3BJ3BJ5Rpm6Tf3HWqCMSHDGuz3WBeuIhz1nMvB1MOsYc_0a-uyYqjtpT0qYU7C9OjgHebUd4BpISG4Y7jHAO3cRWGJmrNpLmrBmn2NM9x9jQsa1f7lIJwUY73jxURs8kNJnTw
 
+import fetch from 'node-fetch';
+
+const response = await fetch('https://api.github.com/users/github');
+const data = await response.json();
+
+console.log(data);
